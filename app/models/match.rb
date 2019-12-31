74b246fe09dbb4cde28b513f56125448275bdf60
Alisher <Allumati@icloud.com>
2019-12-31 14:14:32 -0500

    Managed to push quite a bit on the CLI front

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,4 @@
 class Match < ActiveRecord::Base 
-    attr_accessor :score
     has_and_belongs_to_many :player 
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -4,63 +4,94 @@ require_relative '../config/environment'
 def start
         old_logger = ActiveRecord::Base.logger
         ActiveRecord::Base.logger = nil
+
+        def add_players(user_id)  ##passing user_id as soon as you log in 
+            Player.all.each do |player| 
+                puts player.name
+            end 
+            puts "Please enter a player name you wish to follow"
+            name = gets.chomp 
+            searched_data = Player.find_by(name: name) ##later check if name already exists inside user list
+            Follow.create(player_id: searched_data.id, user_id: user_id)
     
-    def update_my_list #this is the menu that holds add / delete 
+            Follow.all.each do |follow| 
+                if follow.user_id == user_id 
+                    puts "You have followed: " + follow.player.name
+                end
+                end 
+            ##prompt will ask "what is the name of the player you want to find "
+                ##find_name = gets.chomp 
+                ## Player.all.find_by ?
+                ##hey does gets.chomp == find_by name 
+    
+            ###after adding player, returns follow list that includes new player then sends back to follow menu 
+            #follow_menu #this is follow menu
+           follow_menu(user_id)
+        end 
+        
+    def update_my_list(user_id) #this is the menu that holds add / delete 
         prompt = TTY::Prompt.new
         choices = [{name: "Add players", value: 1},{name: 'Delete players', value: 2},{name: 'Go back', value: 3}]
         user_input = prompt.select("Please make your choice", choices)
-        update_my_list_selector(user_input)
+        if user_input == 1 
+            add_players(user_id)
+        elsif user_input == 2
+            delete_players(user_id)
+        elsif user_input == 3 #log out
+            follow_menu(user_id)
+        end
         #add = find_by(name), #delete LIST ALL FOLLOWS, select to delete ie; pulls up prompt with your data 
     end 
     
     
-    def update_my_list_selector(user_input)
-        if user_input == 1 
-        add_players  #add players by name (find_by clause)
-        elsif user_input == 2
-        delete_players #deletes players in a list format 
-        elsif user_input == 3 #go back
-        see_my_list
-        end
-    end
+
     
-    def follow_menu_selector(user_input)
+    def follow_menu_selector(user_input, user_id)
         if user_input == 1 
-            update_my_list  #list of players & search players
+            update_my_list(user_id)  #list of players & search players
         elsif user_input == 2
-            main_menu
+            main_menu(user_id)
         elsif user_input == 3 #log out
             login_screen
         end
     end
-    def see_my_list #FOLLOW_MENU
+    def follow_menu(user_id) #FOLLOW_MENU
         #puts out information regarding list of players that you have
+        #while nil output "you currently aren't following anyone"
+        
+        follow_list =  Follow.all.select {|follow| follow.user_id == user_id}
+        # ##outputs list of follows, prettify it later
+        # if follow_list == nil 
+        #     puts "hey you ain't got no follows"
+        # else
+        #     puts follow_list
+        #end 
         prompt = TTY::Prompt.new
         choices = [{name: "Update my list", value: 1},{name: 'Return to main menu', value: 2},{name: 'Logout', value: 3}]
         user_input = prompt.select("Please make your choice", choices)
-        follow_menu_selector(user_input)
+        follow_menu_selector(user_input, user_id)
         #Follows.where()  ##user_id matches ? // ##current list of players
         
         #AT THE END IT GIVES OPTION TO UPDATE MY LIST LOG OUT OR MAIN MENU
         
     end
-    def main_menu_selector(user_input)
+    def main_menu_selector(user_input, user_id)
         if user_input == 1 
-            see_my_list  #list of players & search players
+            follow_menu(user_id)  #list of players & search players
         elsif user_input == 2
-            update_my_list
+            update_my_list(user_id)
         elsif user_input == 3 #log out
             login_screen
         end
     end
 
-    def main_menu
+    def main_menu(user_id)
         puts "MAIN MENU"
         prompt = TTY::Prompt.new 
         choices = [{name: "See my list", value: 1},{name: 'Update my list', value: 2},{name: 'Logout', value: 3}]
         ###choice # see my list, update my list(select from all, or search for specific players)
         user_input = prompt.select("Please make your choice", choices)
-        main_menu_selector(user_input)
+        main_menu_selector(user_input, user_id)
     end
 
     def login_screen
@@ -100,7 +131,7 @@ def start
                 puts "Incorrect password, to verify that you are human, please do this captcha"
                     user = nil 
             end 
-            main_menu
+            main_menu(user.id)
     end 
  
     def create_account ###if acc exists, loop w/ "create a different unique name"
@@ -123,20 +154,12 @@ def start
     end 
 
     def print_follow_list #with new player 
-        
+        #call it after adding a new player to your list
 
         #returns to update_my_list
     end 
 
-    def add_players
-            ##prompt will ask "what is the name of the player you want to find "
-            ##find_name = gets.chomp 
-            ## Player.all.find_by ?
-            ##hey does gets.chomp == find_by name 
 
-        ###after adding player, returns follow list that includes new player then sends back to follow menu 
-        #see_my_list #this is follow menu
-    end 
     def delete_players
         
         ## returns prompt of list of all players you currently have followed
@@ -160,3 +183,4 @@ def prints_out_all_matches
     end 
     match_array
 end 
+=end 
